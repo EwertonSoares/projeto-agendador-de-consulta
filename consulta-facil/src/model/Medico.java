d735b38b865a1ca1a10d0f0e8f9417c3ace60368
Vinicius-MELI <vinicius.morais@mercadolivre.com>
2023-01-17 14:11:01 -0300

    inclusao das classes model

---
+++ consulta-facil/src/model/Medico.java
@@ -0,0 +1,38 @@
+public class Medico {
+
+
+
+    private int crm;
+
+    private String especialidade;
+
+
+
+    public Medico(int crm, String especialidade) {
+        this.crm = crm;
+        this.especialidade = especialidade;
+    }
+
+
+    public int getCrm() {
+        return crm;
+    }
+
+    public void setCrm(int crm) {
+        this.crm = crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+
+
+
+
+
+}
